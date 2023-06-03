a6ebf12984650b29de8ea5c60acf4366747e839c
Kibnakamoto <kibnakanoto@protonmail.com>
2023-06-03 10:52:33 -0400

    subtraction is working

--- main.cpp
+++ main.cpp
@@ -16,8 +16,9 @@ int main()
 	std::cout << "int num: " << num;
 	std::cout << "\nhex num2: " << std::hex << num2;
 	auto num3 = num + num2;
+	uint256_t num4 = std::string("1343958435345038457409573906905768787408560675098704249822"); // 232-bit hex
 	std::cout << "\nnum+num2: " << std::hex << num3 << std::endl;
-	std::cout << "num - num2: " << (num - num2);
+	std::cout << "num - num2: " << (num - num4); // 0xff3cbe3fe20d00e1ac04edc0e39f439f0c9024aeb0c8d6f0c14ffbe3f
 	std::cout << std::endl;
 	return 0;
 }

782a2a39f7eff14ca51affa7a33074205b30c05f
Kibnakamoto <kibnakanoto@protonmail.com>
2023-12-09 18:01:35 -0500

    added info on top of files

--- bigint.h
+++ bigint.h
@@ -1,3 +1,19 @@
+ /* Copyright (c) 2022 Taha
+  * this program is free software: you can redistribute it and/or modify
+  * it under the terms of the gnu general public license as published by
+  * the free software foundation, either version 3 of the license, or
+  * (at your option) any later version.
+  * this program is distributed in the hope that it will be useful,
+  * but without any warranty; without even the implied warranty of
+  * merchantability or fitness for a particular purpose.  see the
+  * gnu general public license for more details.
+  * you should have received a copy of the gnu general public license
+  * along with this program.  if not, see <https://www.gnu.org/licenses/>.
+  *
+  * Author: Taha
+  * Date: 2022, Sep 10
+  */
+
 #ifndef BIGINT_H
 #define BIGINT_H
 
